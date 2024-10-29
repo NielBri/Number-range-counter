47b47ea8fc92259d63099463a037d92ea7d3fb7c
NielBri <briones.nielmatthew@gmail.com>
2024-10-29 15:16:53 +0800

    This is the first commit pseudo code

---
+++ main_project/main_file.py
@@ -0,0 +1,6 @@
+# List
+# Make a function that allows and detects a number
+# Put the numbers in an array
+# Make a function that seperates the numbers in the given ranges
+# Make a function that counts how many numbers are in each range
+# Print
