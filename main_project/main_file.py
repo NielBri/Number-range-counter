ea2ae3993d3d0d9bcb1f6937bba19d8e777c23bb
NielBri <briones.nielmatthew@gmail.com>
2024-10-29 16:21:50 +0800

    Added a function that separates each num in the given ranges

--- main_project/main_file.py
+++ main_project/main_file.py
@@ -18,5 +18,12 @@ while True:
         print("Please input a valid number. ")
 
 # Make a function that separates the numbers in the given ranges
+range1 = [num for num in numbers if 1 <= num <= 10]
+range2 = [num for num in numbers if 11 <= num <= 20]
+range3 = [num for num in numbers if 21 <= num <= 30]
+range4 = [num for num in numbers if 31 <= num <= 40]
+range5 = [num for num in numbers if 41 <= num <= 50]
+
+print(range1)
 # Make a function that counts how many numbers are in each range
 # Print
